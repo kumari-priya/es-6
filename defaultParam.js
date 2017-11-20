aedd242affc5713a032989c0e1de4b9f6c8b362d
Kumari Priya <priya@goel.pw>
2017-11-19 17:46:10 -0800

    default params

---
+++ defaultParam.js
@@ -0,0 +1,14 @@
+function sayMsg(msg='This is a default message.') {
+  console.log(msg);
+}
+
+sayMsg(); // This is a default message.
+sayMsg('This is a different message!'); // This is a different message!
+
+// function sayMsg(msg){
+//   var msg = msg || 'This is a default message.';
+//   console.log(msg);
+// }
+//
+// sayMsg();  // This is a default message.
+// sayMsg('This is a different message!'); // This is a different message!
