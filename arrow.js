f0130784b15b7fea0ff0f38a6771b398c14152b7
Kumari Priya <priya@goel.pw>
2017-11-19 17:45:43 -0800

    arrow function

---
+++ arrow.js
@@ -0,0 +1,13 @@
+'use strict'
+let square = x => x * x;
+let add = (a, b) => a + b;
+let pi = () => 3.1415;
+
+console.log(square(5)); // 25
+console.log(add(3, 4)); // 7
+console.log(pi()); // 3.1415
+
+
+// var square = function(x) { return x * x; };
+// var add = function(a, b) { return a + b; };
+// var pi = function() { return 3.1415; };
